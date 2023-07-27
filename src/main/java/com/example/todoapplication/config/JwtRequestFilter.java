88bb82b715ef48e30842f69dace84a75e9c89c1e
MsHitai <mmalafeeva@yandex.ru>
2023-07-27 15:22:00 +0300

    fix: изменить выбрасывание некоторых исключений

--- src/main/java/com/example/todoapplication/config/JwtRequestFilter.java
+++ src/main/java/com/example/todoapplication/config/JwtRequestFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
